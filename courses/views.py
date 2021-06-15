63d8b6550f01b03c6746e3b8ab1c3b14075ff51b
81135998191 <81135998191@dti-desen15>
2021-06-15 18:07:04 -0300

    LISTANDO CURSOS
    Listando os assuntos e cursos disponiveis.

--- courses/views.py
+++ courses/views.py
@@ -5,11 +5,14 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+from django.db.models import Count
 
 
 class OwnerMixin(object):
@@ -170,8 +173,24 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         return self.render_json_response({'saved': 'OK'})
 
 
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=object)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
 
 
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
 
 
