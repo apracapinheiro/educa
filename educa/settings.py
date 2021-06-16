af0cee8328e6d89e7ade10acf7c8d162dd6d0416
81135998191 <81135998191@dti-desen15>
2021-06-16 17:04:45 -0300

    LISTAGEM DE CONTEUDO PARA ALUNOS
    Listagem de conteudos de cursos para alunos.

--- educa/settings.py
+++ educa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.urls import reverse_lazy
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
+    'embed_video',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- students/views.py
+++ students/views.py
@@ -48,7 +48,7 @@ class StudentCourseListView(LoginRequiredMixin,ListView):
 
 class StudentCourseDetailView(DetailView):
     model = Course
-    template_name = 'students/course/detail.html
+    template_name = 'students/course/detail.html'
 
     def get_queryset(self):
         qs = super().get_queryset()
