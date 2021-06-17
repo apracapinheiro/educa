faeb0329bc2fb30b839f32a1bae703a9f28e27c3
81135998191 <81135998191@dti-desen15>
2021-06-17 15:44:23 -0300

    CRIANDO UMA API
    viewset e roteadores

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework import viewsets
 
 
 class SubjectListView(generics.ListAPIView):
@@ -27,3 +29,8 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
