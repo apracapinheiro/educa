b7b8e0cfdf44cc4b3b71f1b473cc9e4fa637bdff
81135998191 <81135998191@dti-desen15>
2021-06-17 15:12:02 -0300

    CRIANDO UMA API
    Listando cursos e modulos
    Realizando inscricao no curso pela API

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
-from ..models import Subject
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
+
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class SubjectListView(generics.ListAPIView):
@@ -11,3 +15,11 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
